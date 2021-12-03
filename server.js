c9c69e3a233ea197f3bacff928715bd9392e6464
karlmerhi <59775820+karlmerhi@users.noreply.github.com>
2021-12-03 12:19:47 -0500

    commit

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
-* WEB322 – Assignment 06
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
-*
-* Name:__Damirkhon Yodgorov___ Student ID:__108364175__ Date: 30.11.2018
-*
-* Online (Heroku) Link: https://pacific-garden-26189.herokuapp.com/
-*
-********************************************************************************/ 
 
 var express = require('express');
 var data_service = require('./data-service');
